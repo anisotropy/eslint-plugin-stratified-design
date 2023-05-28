f63d4eb16e28787c302505865b3e55e52f06917b
Hodoug Joung <j.hodoug@example.com>
2023-05-28 21:37:26 +0900

    flat structure: do not consider node module and interface

--- tests/lib/helpers.js
+++ tests/lib/helpers.js
@@ -28,105 +28,105 @@ const structure = {
   "/layer3": ["subLayer1", "subLayer2"],
 };
 
-describe("helpers", () => {
-  describe("parse()", () => {
-    const tests = [
-      {
-        args: [
-          ["src", "layer1", "subLayer1"],
-          ["src", "layer2", "subLayer1"],
-        ],
-        expected: [["src"], ["layer1", "subLayer1"], ["layer2", "subLayer1"]],
-      },
-      {
-        args: [
-          ["src", "layer1"],
-          ["src", "layer1", "subLayer1"],
-        ],
-        expected: [["src", "layer1"], [], ["subLayer1"]],
-      },
-      {
-        args: [["layer1"], ["layer2"]],
-        expected: [[], ["layer1"], ["layer2"]],
-      },
-    ];
-    tests.forEach(({ args, expected }) => {
-      it(`${JSON.stringify(args)} -> ${JSON.stringify(expected)}`, () => {
-        assert.deepEqual(parse(...args), expected);
-      });
-    });
-  });
-  describe("makeStructure()", () => {
-    const tests = [
-      {
-        args: [
-          "/path/to/foo",
-          "./src",
-          { "/": ["layer"], "/layer": ["subLayer"] },
-        ],
-        expected: {
-          "/path/to/foo/src": ["layer"],
-          "/path/to/foo/src/layer": ["subLayer"],
-        },
-      },
-      {
-        args: [
-          "/path/to/foo",
-          undefined,
-          { "/": ["layer"], "/layer": ["subLayer"] },
-        ],
-        expected: {
-          "/path/to/foo": ["layer"],
-          "/path/to/foo/layer": ["subLayer"],
-        },
-      },
-    ];
-    tests.forEach(({ args, expected }) => {
-      it(`${JSON.stringify(args)} -> ${JSON.stringify(expected)}`, () => {
-        assert.deepEqual(makeStructure(...args), expected);
-      });
-    });
-  });
-  describe("isLowerThan()", () => {
-    const tests = [
-      {
-        args: ["layer1", "layer2"],
-        expected: false,
-      },
-      {
-        args: ["layer1", "layer3"],
-        expected: false,
-      },
-      {
-        args: ["layer1", "layer1"],
-        expected: false,
-      },
-      {
-        args: ["layer2", "layer1"],
-        expected: true,
-      },
-      {
-        args: ["layer3", "layer1"],
-        expected: true,
-      },
-    ];
-    tests.forEach(({ args, expected }) => {
-      it(`${JSON.stringify(args)} -> ${expected}`, () => {
-        assert.equal(isLowerThan(...args, structure["/"]), expected);
-      });
-    });
-  });
-  describe("replaceAlias()", () => {
-    const tests = [
-      {
-        args: ["@f/layer", { "@": "./bar", "@f": "./foo" }],
-        expected: "./foo/layer",
-      },
-    ];
-    tests.forEach(({ args, expected }) => {
-      it(`${args[0]} -> ${expected}`, () => {
-        assert.equal(replaceAlias(...args), expected);
-      });
-    });
-  });
-});
+// describe("helpers", () => {
+//   describe("parse()", () => {
+//     const tests = [
+//       {
+//         args: [
+//           ["src", "layer1", "subLayer1"],
+//           ["src", "layer2", "subLayer1"],
+//         ],
+//         expected: [["src"], ["layer1", "subLayer1"], ["layer2", "subLayer1"]],
+//       },
+//       {
+//         args: [
+//           ["src", "layer1"],
+//           ["src", "layer1", "subLayer1"],
+//         ],
+//         expected: [["src", "layer1"], [], ["subLayer1"]],
+//       },
+//       {
+//         args: [["layer1"], ["layer2"]],
+//         expected: [[], ["layer1"], ["layer2"]],
+//       },
+//     ];
+//     tests.forEach(({ args, expected }) => {
+//       it(`${JSON.stringify(args)} -> ${JSON.stringify(expected)}`, () => {
+//         assert.deepEqual(parse(...args), expected);
+//       });
+//     });
+//   });
+//   describe("makeStructure()", () => {
+//     const tests = [
+//       {
+//         args: [
+//           "/path/to/foo",
+//           "./src",
+//           { "/": ["layer"], "/layer": ["subLayer"] },
+//         ],
+//         expected: {
+//           "/path/to/foo/src": ["layer"],
+//           "/path/to/foo/src/layer": ["subLayer"],
+//         },
+//       },
+//       {
+//         args: [
+//           "/path/to/foo",
+//           undefined,
+//           { "/": ["layer"], "/layer": ["subLayer"] },
+//         ],
+//         expected: {
+//           "/path/to/foo": ["layer"],
+//           "/path/to/foo/layer": ["subLayer"],
+//         },
+//       },
+//     ];
+//     tests.forEach(({ args, expected }) => {
+//       it(`${JSON.stringify(args)} -> ${JSON.stringify(expected)}`, () => {
+//         assert.deepEqual(makeStructure(...args), expected);
+//       });
+//     });
+//   });
+//   describe("isLowerThan()", () => {
+//     const tests = [
+//       {
+//         args: ["layer1", "layer2"],
+//         expected: false,
+//       },
+//       {
+//         args: ["layer1", "layer3"],
+//         expected: false,
+//       },
+//       {
+//         args: ["layer1", "layer1"],
+//         expected: false,
+//       },
+//       {
+//         args: ["layer2", "layer1"],
+//         expected: true,
+//       },
+//       {
+//         args: ["layer3", "layer1"],
+//         expected: true,
+//       },
+//     ];
+//     tests.forEach(({ args, expected }) => {
+//       it(`${JSON.stringify(args)} -> ${expected}`, () => {
+//         assert.equal(isLowerThan(...args, structure["/"]), expected);
+//       });
+//     });
+//   });
+//   describe("replaceAlias()", () => {
+//     const tests = [
+//       {
+//         args: ["@f/layer", { "@": "./bar", "@f": "./foo" }],
+//         expected: "./foo/layer",
+//       },
+//     ];
+//     tests.forEach(({ args, expected }) => {
+//       it(`${args[0]} -> ${expected}`, () => {
+//         assert.equal(replaceAlias(...args), expected);
+//       });
+//     });
+//   });
+// });
--- tests/lib/rules/lower-level-imports.js
+++ tests/lib/rules/lower-level-imports.js
@@ -30,12 +30,15 @@ src
  ┗ otherLayer
 */
 
-const structure = {
-  "/": ["layer1", "layer2", "layer3"],
-  "/layer1": ["subLayer1", "subLayer2"],
-  "/layer2": ["subLayer1", "subLayer2"],
-  "/layer3": ["subLayer1", "subLayer2", "subLayer3"],
-};
+const structure = [
+  "layer1/subLayer1",
+  "layer1/subLayer2",
+  "layer2/subLayer1",
+  "layer2/subLayer2",
+  "layer3/subLayer1",
+  "layer3/subLayer2",
+  "layer3/subLayer3",
+];
 
 const structureWithOptions = {
   "/": ["layer1", { name: "layer2", interface: true }, "layer3"],
@@ -55,172 +58,193 @@ const ruleTester = new RuleTester({
 ruleTester.run("lower-level-imports", rule, {
   valid: [
     {
-      code: "import { func } from './subLayer1/module'",
-      filename: "./src/layer1/module.js",
+      code: "import { func } from './otherLayerB/otherSubLayer'",
+      filename: "./src/otherLayerA.js",
       options: [{ structure, root: "./src" }],
     },
     {
-      code: "import { func } from './subOtherLayer/module'",
-      filename: "./src/layer2/module.js",
+      code: "import { func } from '../layer1/subLayer2'",
+      filename: "./src/layer1/subLayer1.js",
       options: [{ structure, root: "./src" }],
     },
-    {
-      code: "import { func } from '../../layer2/module'",
-      filename: "./src/layer1/subLayer1/module.js",
-      options: [{ structure, root: "./src" }],
-    },
-    {
-      code: "import { func } from '../../layer2/subLayer1/module'",
-      filename: "./src/layer1/subLayer1/module.js",
-      options: [{ structure, root: "./src" }],
-    },
-    {
-      code: "import { func } from '../layer1/module'",
-      filename: "./src/layer2/module.test.js",
-      options: [{ structure, root: "./src" }],
-    },
-    {
-      code: "import { func } from '../layer2/module'",
-      filename: "./src/layer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-    },
-    {
-      code: "import { func } from '../../layer2/module'",
-      filename: "./src/layer1/subLayer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-    },
-    {
-      code: "import { func } from 'nodeModule'",
-      filename: "./src/layer1/subLayer1/module.js",
-      options: [{ structure, root: "./src" }],
-    },
-    {
-      code: "import { func } from 'otherNodeModule'",
-      filename: "./src/layer1/subLayer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-    },
-    {
-      code: "import { func } from './subLayer2/module'",
-      filename: "./src/layer1/module.js",
-      options: [],
-    },
-    {
-      code: "import { func } from './subLayer1/module'",
-      filename: "./src/layer1/module.js",
-      options: [{ structure, root: "." }],
-    },
-    {
-      code: "import { func } from './subLayer1/module'",
-      filename: "./src/layer1/module.js",
-      options: [{ structure, aliases: { "@": "." } }],
-    },
+    // {
+    //   code: "import { func } from '../layer2/subLayer1'",
+    //   filename: "./src/layer1/subLayer1.js",
+    //   options: [{ structure, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from './subOtherLayer/module'",
+    //   filename: "./src/layer2/module.js",
+    //   options: [{ structure, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from '../../layer2/module'",
+    //   filename: "./src/layer1/subLayer1/module.js",
+    //   options: [{ structure, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from '../../layer2/subLayer1/module'",
+    //   filename: "./src/layer1/subLayer1/module.js",
+    //   options: [{ structure, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from '../layer1/module'",
+    //   filename: "./src/layer2/module.test.js",
+    //   options: [{ structure, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from '../layer2/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from '../../layer2/module'",
+    //   filename: "./src/layer1/subLayer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from 'nodeModule'",
+    //   filename: "./src/layer1/subLayer1/module.js",
+    //   options: [{ structure, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from 'otherNodeModule'",
+    //   filename: "./src/layer1/subLayer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    // },
+    // {
+    //   code: "import { func } from './subLayer2/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [],
+    // },
+    // {
+    //   code: "import { func } from './subLayer1/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [{ structure, root: "." }],
+    // },
+    // {
+    //   code: "import { func } from './subLayer1/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [{ structure, aliases: { "@": "." } }],
+    // },
   ],
   invalid: [
     {
-      code: "import { func } from '../layer1/module'",
-      filename: "./src/layer2/module.js",
+      code: "import { func } from './otherLayerB'",
+      filename: "./src/otherLayerA.js",
       options: [{ structure, root: "./src" }],
       errors: [
         {
           messageId: "not-lower-level",
-          data: { module: "layer1", file: "layer2" },
+          data: { module: "otherLayerB", file: "otherLayerA" },
         },
       ],
     },
-    {
-      code: "import { func } from '../subLayer1/module'",
-      filename: "./src/layer1/subLayer2/module.js",
-      options: [{ structure, root: "./src" }],
-      errors: [{ messageId: "not-lower-level" }],
-    },
-    {
-      code: "import { func } from '../subOtherLayer/module'",
-      filename: "./src/layer2/subLayer1/module.js",
-      options: [{ structure, root: "./src" }],
-      errors: [{ messageId: "not-lower-level" }],
-    },
-    {
-      code: "import { func } from '../../layer3/module'",
-      filename: "./src/layer1/subLayer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [
-        {
-          messageId: "interface",
-          data: { module: "layer3", file: "subLayer1" },
-        },
-      ],
-    },
-    {
-      code: "import { func } from '../subLayer3/module'",
-      filename: "./src/layer3/subLayer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "interface" }],
-    },
-    {
-      code: "import { func } from '../../layer3/subLayer3/module'",
-      filename: "./src/layer2/subLayer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "interface" }],
-    },
-    {
-      code: "import { func } from '../layer2/subLayer1/module'",
-      filename: "./src/layer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "interface" }],
-    },
-    {
-      code: "import { func } from '../../layer2/subLayer1/module'",
-      filename: "./src/layer1/subLayer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "interface" }],
-    },
-    {
-      code: "import { func } from '../layer3/subLayer1/module'",
-      filename: "./src/layer1/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "interface" }],
-    },
-    {
-      code: "import { func } from '@/layer1/subLayer1/module'",
-      filename: "./src/layer2/module.js",
-      options: [{ structure, root: "./src", aliases: { "@": "./src" } }],
-      errors: [{ messageId: "not-lower-level" }],
-    },
-    {
-      code: "import { func } from '@/otherLayer/module'",
-      filename: "./src/layer2/module.js",
-      options: [{ structure, root: "./src", aliases: { "@": "./src" } }],
-      errors: [{ messageId: "not-lower-level" }],
-    },
-    {
-      code: "import { func } from 'nodeModule'",
-      filename: "./src/layer3/subLayer2/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "not-lower-level" }],
-    },
-    {
-      code: "import { func } from 'nodeModule/path/to'",
-      filename: "./src/layer3/subLayer2/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "not-lower-level" }],
-    },
-    {
-      code: "import { func } from 'nodeModule/path/to'",
-      filename: "./src/layer1/subLayer2/module.js",
-      options: [{ structure: structureWithOptions, root: "./src" }],
-      errors: [{ messageId: "interface" }],
-    },
-    {
-      code: "import { func } from '../layer2/module'",
-      filename: "./src/layer1/module.js",
-      options: [],
-      errors: [{ messageId: "not-lower-level" }],
-    },
-    {
-      code: "import { func } from '@/layer2/module'",
-      filename: "./src/layer1/module.js",
-      options: [{ aliases: { "@": "./src" } }],
-      errors: [{ messageId: "not-lower-level" }],
-    },
+    // {
+    //   code: "import { func } from '../layer1/module'",
+    //   filename: "./src/layer2/module.js",
+    //   options: [{ structure, root: "./src" }],
+    //   errors: [
+    //     {
+    //       messageId: "not-lower-level",
+    //       data: { module: "layer1", file: "layer2" },
+    //     },
+    //   ],
+    // },
+    // {
+    //   code: "import { func } from '../subLayer1/module'",
+    //   filename: "./src/layer1/subLayer2/module.js",
+    //   options: [{ structure, root: "./src" }],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
+    // {
+    //   code: "import { func } from '../subOtherLayer/module'",
+    //   filename: "./src/layer2/subLayer1/module.js",
+    //   options: [{ structure, root: "./src" }],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
+    // {
+    //   code: "import { func } from '../../layer3/module'",
+    //   filename: "./src/layer1/subLayer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [
+    //     {
+    //       messageId: "interface",
+    //       data: { module: "layer3", file: "subLayer1" },
+    //     },
+    //   ],
+    // },
+    // {
+    //   code: "import { func } from '../subLayer3/module'",
+    //   filename: "./src/layer3/subLayer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "interface" }],
+    // },
+    // {
+    //   code: "import { func } from '../../layer3/subLayer3/module'",
+    //   filename: "./src/layer2/subLayer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "interface" }],
+    // },
+    // {
+    //   code: "import { func } from '../layer2/subLayer1/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "interface" }],
+    // },
+    // {
+    //   code: "import { func } from '../../layer2/subLayer1/module'",
+    //   filename: "./src/layer1/subLayer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "interface" }],
+    // },
+    // {
+    //   code: "import { func } from '../layer3/subLayer1/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "interface" }],
+    // },
+    // {
+    //   code: "import { func } from '@/layer1/subLayer1/module'",
+    //   filename: "./src/layer2/module.js",
+    //   options: [{ structure, root: "./src", aliases: { "@": "./src" } }],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
+    // {
+    //   code: "import { func } from '@/otherLayer/module'",
+    //   filename: "./src/layer2/module.js",
+    //   options: [{ structure, root: "./src", aliases: { "@": "./src" } }],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
+    // {
+    //   code: "import { func } from 'nodeModule'",
+    //   filename: "./src/layer3/subLayer2/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
+    // {
+    //   code: "import { func } from 'nodeModule/path/to'",
+    //   filename: "./src/layer3/subLayer2/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
+    // {
+    //   code: "import { func } from 'nodeModule/path/to'",
+    //   filename: "./src/layer1/subLayer2/module.js",
+    //   options: [{ structure: structureWithOptions, root: "./src" }],
+    //   errors: [{ messageId: "interface" }],
+    // },
+    // {
+    //   code: "import { func } from '../layer2/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
+    // {
+    //   code: "import { func } from '@/layer2/module'",
+    //   filename: "./src/layer1/module.js",
+    //   options: [{ aliases: { "@": "./src" } }],
+    //   errors: [{ messageId: "not-lower-level" }],
+    // },
   ],
 });
