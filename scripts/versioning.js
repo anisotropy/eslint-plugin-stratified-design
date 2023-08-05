2c89a783552fc13bd4ab15d7c9a7cea0581c91b2
Hodoug Joung <j.hodoug@example.com>
2023-08-06 08:11:44 +0900

    30806-1 push in versioning.js

--- scripts/versioning.js
+++ scripts/versioning.js
@@ -1,16 +1,25 @@
 const fs = require("fs");
+const { execSync } = require("child_process");
+
+const noChange = "__NO_CHANGE__";
+
+function exec(command) {
+  const output = execSync(command, { encoding: "utf-8" });
+  console.log(output);
+}
 
 function updatePackageVersion() {
   const githubEventPath = process.env.GITHUB_EVENT_PATH || "";
   const packageJsonPath = "package.json";
 
-  const version = (() => {
+  const { version, targetCommitish } = (() => {
     try {
       const githubEventString = fs.readFileSync(githubEventPath, "utf-8");
       const githubEvent = JSON.parse(githubEventString);
       const tagName = githubEvent.release.tag_name;
+      const targetCommitish = githubEvent.release.target_commitish;
       const version = tagName.replace(/^v(.*)/, "$1");
-      return version;
+      return { version, targetCommitish };
     } catch (error) {
       throw new Error("Error parsing GITHUB_EVENT_PATH:", error);
     }
@@ -19,6 +28,7 @@ function updatePackageVersion() {
   const packageJson = (() => {
     try {
       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+      if (packageJson.version === version) return noChange;
       packageJson.version = version;
       const updatedPackageJson = JSON.stringify(packageJson, null, 2) + "\n";
       return updatedPackageJson;
@@ -27,12 +37,24 @@ function updatePackageVersion() {
     }
   })();
 
+  if (packageJson === noChange) return;
+
   try {
     fs.writeFileSync(packageJsonPath, packageJson, "utf8");
-    console.log(`Successfully updated version to ${version}`);
   } catch (error) {
     throw new Error("Error writing package.json:", error);
   }
+
+  try {
+    exec(`git config --local user.email "action@github.com"`);
+    exec(`git config --local user.name "GitHub Action"`);
+    exec(`git commit -a -m "Update package version: ${version}"`);
+    exec(`git push origin HEAD:${targetCommitish}`);
+  } catch (error) {
+    throw new Error("Error executing push package.json", error);
+  }
+
+  console.log(`Successfully updated version to ${version}`);
 }
 
 updatePackageVersion();
