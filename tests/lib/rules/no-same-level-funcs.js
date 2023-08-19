f78ac128dabbf13e42ddabb5914b6cef9a49c3c4
Hodoug Joung <j.hodoug@example.com>
2023-08-19 10:58:38 +0900

    30819 no-same-level-funcs: different levels in a same file

--- tests/lib/rules/no-same-level-funcs.js
+++ tests/lib/rules/no-same-level-funcs.js
@@ -44,6 +44,30 @@ ruleTester.run("no-same-level-funcs", rule, {
       filename: "./src/foo.js",
       options: [{ include: ["**/src/**/*.*"], exclude: ["**/foo.js"] }],
     },
+    {
+      code: "// @level 2\nfunction func2(){};\n// @level 1\nfunction func1(){ func2(); }",
+      filename: "./src/foo.js",
+    },
+    {
+      code: "// @level 2\nconst func2 = () => {};\n// @level 1\nfunction func1(){ func2(); }",
+      filename: "./src/foo.js",
+    },
+    {
+      code: "// @level 2\nconst func2 = () => {};\n// @level 1\nconst func1 = () => func2();",
+      filename: "./src/foo.js",
+    },
+    {
+      code: "/*@level 2*/\nconst func2 = () => {};\n/*@level 1*/\nconst func1 = () => func2();",
+      filename: "./src/foo.js",
+    },
+    {
+      code: "// @level 2 something\nconst func2 = () => {};\n// @level 1 something\nconst func1 = () => func2();",
+      filename: "./src/foo.js",
+    },
+    {
+      code: "/*\n@level 2\nsomething\n*/\nconst func2 = () => {};\n/*something\n@level 1\n*/\nconst func1 = () => func2();",
+      filename: "./src/foo.js",
+    },
   ],
   invalid: [
     {
@@ -68,6 +92,26 @@ ruleTester.run("no-same-level-funcs", rule, {
     },
     {
       code: "const func1 = function func1(){}; const func2 = function func2(){ func1(); }",
+      filename: "./src/foo.js",
+      errors: [{ messageId: "no-same-level-funcs", data: { func: "func1" } }],
+    },
+    {
+      code: "const fn = () => 1; const value = fn()",
+      filename: "./src/foo.js",
+      errors: [{ messageId: "no-same-level-funcs", data: { func: "fn" } }],
+    },
+    {
+      code: "const fnByHof = hof(() => 1); const value = fnByHof()",
+      filename: "./src/foo.js",
+      errors: [{ messageId: "no-same-level-funcs", data: { func: "fnByHof" } }],
+    },
+    {
+      code: "const fnByHof = hof(() => 1); const fn = fnByHof(() => 1)",
+      filename: "./src/foo.js",
+      errors: [{ messageId: "no-same-level-funcs", data: { func: "fnByHof" } }],
+    },
+    {
+      code: "// @level 1\nfunction func1(){};\n// @level 2\nfunction func2(){ func1(); }",
       filename: "./src/foo.js",
       errors: [{ messageId: "no-same-level-funcs", data: { func: "func1" } }],
     },
