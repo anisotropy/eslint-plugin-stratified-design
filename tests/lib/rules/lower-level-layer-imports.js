c868f8d150b908b910e4731b77f38b9750f9bc44
Hodoug Joung <j.hodoug@example.com>
2023-08-20 15:15:50 +0900

    30820 lower-level-layer-imports: includes parent structure

--- tests/lib/rules/lower-level-layer-imports.js
+++ tests/lib/rules/lower-level-layer-imports.js
@@ -26,7 +26,20 @@ const RuleTester = require("eslint").RuleTester;
   ["layerF"],
   ["layerG", "layerH"],
   ["layerI"],
-  ["layerJ/entryA", "layerJ/entryB"]
+  ["layerJ/entryA", "layerJ/entryB"],
+  ["layerK"]
+]
+
+// layerB/.stratified.json
+[
+  ["layerBA"],
+  ["layerBB"]
+]
+
+// layerJ/.stratified.json
+[
+  ["entryJA", "entryJB"],
+  ["layerJC"]
 ]
 */
 
@@ -38,34 +51,59 @@ ruleTester.run("lower-level-layer-imports", rule, {
   valid: [
     {
       code: "import { func } from './layerB'",
-      filename: "./mocked/layerA.js",
+      filename: "./mocked/lower-level-layer-imports/layerA.js",
       options: [],
     },
     {
       code: "import { func } from './layerC'",
-      filename: "./mocked/layerA.js",
+      filename: "./mocked/lower-level-layer-imports/layerA.js",
       options: [],
     },
     {
       code: "import { func } from 'nodeModuleE'",
-      filename: "./mocked/layerD.js",
+      filename: "./mocked/lower-level-layer-imports/layerD.js",
       options: [],
     },
     {
       code: "import { func } from './layerH'",
-      filename: "./mocked/layerF.js",
+      filename: "./mocked/lower-level-layer-imports/layerF.js",
       options: [],
     },
     {
       code: "import { func } from './layerI'",
-      filename: "./mocked/layerH.js",
+      filename: "./mocked/lower-level-layer-imports/layerH.js",
+      options: [],
+    },
+    {
+      code: "import { func } from './layerBB'",
+      filename: "./mocked/lower-level-layer-imports/layerB/layerBA.js",
+      options: [],
+    },
+    {
+      code: "import { func } from '../layerC'",
+      filename: "./mocked/lower-level-layer-imports/layerB/layerBA.js",
+      options: [],
+    },
+    {
+      code: "import { func } from './layerJC'",
+      filename: "./mocked/lower-level-layer-imports/layerJ/entryJA.js",
+      options: [],
+    },
+    {
+      code: "import { func } from './layerJC'",
+      filename: "./mocked/lower-level-layer-imports/layerJ/entryJB.js",
+      options: [],
+    },
+    {
+      code: "import { func } from '../layerK'",
+      filename: "./mocked/lower-level-layer-imports/layerJ/layerJC.js",
       options: [],
     },
   ],
   invalid: [
     {
       code: "import { func } from './layerA'",
-      filename: "./mocked/layerB.js",
+      filename: "./mocked/lower-level-layer-imports/layerB.js",
       options: [],
       errors: [
         {
@@ -76,12 +114,56 @@ ruleTester.run("lower-level-layer-imports", rule, {
     },
     {
       code: "import { func } from './layerD'",
-      filename: "./mocked/layerB.js",
+      filename: "./mocked/lower-level-layer-imports/layerB.js",
       options: [],
       errors: [
         {
           messageId: "barrier",
           data: { module: "layerD", file: "layerB" },
+        },
+      ],
+    },
+    {
+      code: "import { func } from './layerBA'",
+      filename: "./mocked/lower-level-layer-imports/layerB/layerBB.js",
+      options: [],
+      errors: [
+        {
+          messageId: "not-lower-level",
+          data: { module: "layerBA", file: "layerBB" },
+        },
+      ],
+    },
+    {
+      code: "import { func } from '../layerA'",
+      filename: "./mocked/lower-level-layer-imports/layerB/layerBA.js",
+      options: [],
+      errors: [
+        {
+          messageId: "not-registered",
+          data: { module: "layerA", file: "layerBA" },
+        },
+      ],
+    },
+    {
+      code: "import { func } from './entryJA'",
+      filename: "./mocked/lower-level-layer-imports/layerJ/layerJC.js",
+      options: [],
+      errors: [
+        {
+          messageId: "not-lower-level",
+          data: { module: "entryJA", file: "layerJC" },
+        },
+      ],
+    },
+    {
+      code: "import { func } from '../layerI'",
+      filename: "./mocked/lower-level-layer-imports/layerJ/layerJC.js",
+      options: [],
+      errors: [
+        {
+          messageId: "not-registered",
+          data: { module: "layerI", file: "layerJC" },
         },
       ],
     },
