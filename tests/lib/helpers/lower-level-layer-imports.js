5f1de7f2d5e279ed8b7e8b09bf85ad9a81ff139f
Hodoug Joung <j.hodoug@example.com>
2023-08-20 23:26:16 +0900

    30820 lower-level-layer-imports: test helpers

--- tests/lib/helpers/lower-level-layer-imports.js
+++ tests/lib/helpers/lower-level-layer-imports.js
@@ -11,7 +11,13 @@
 const assert = require("assert");
 const {
   findLevel,
+  toStructure,
+  createAliases,
+  replaceAlias,
 } = require("../../../lib/helpers/lowerLevelLayerImports/2 layer");
+const {
+  createModulePath,
+} = require("../../../lib/helpers/lowerLevelLayerImports/1 layer");
 
 //------------------------------------------------------------------------------
 // Tests
@@ -20,6 +26,65 @@ const {
 // TODO: more test for helper
 
 describe("helpers/lower-level-layer-imports", () => {
+  describe("toStructure()", () => {
+    const fileDir = "/src";
+    const testCases = [
+      { rawStructure: [["layer"]], structure: [[{ name: "/src/layer" }]] },
+      {
+        rawStructure: [[{ name: "layer" }]],
+        structure: [[{ name: "/src/layer" }]],
+      },
+      {
+        rawStructure: [[{ name: "layer", barrier: true }]],
+        structure: [[{ name: "/src/layer", barrier: true }]],
+      },
+    ];
+    testCases.forEach(({ rawStructure, structure }) => {
+      it(`${rawStructure} -> ${structure}`, () => {
+        assert.deepEqual(toStructure(rawStructure, fileDir), structure);
+      });
+    });
+  });
+
+  describe("createAliases()", () => {
+    const testCases = [
+      {
+        rawAliases: { "@/": "./src/" },
+        aliases: [{ alias: "@/", path: "./src/" }],
+      },
+      {
+        rawAliases: { "@/": "./src/", "@layer/": "./src/layer/" },
+        aliases: [
+          { alias: "@layer/", path: "./src/layer/" },
+          { alias: "@/", path: "./src/" },
+        ],
+      },
+    ];
+    testCases.forEach(({ rawAliases, aliases }) => {
+      it(`${rawAliases} -> ${aliases}`, () => {
+        assert.deepEqual(createAliases(rawAliases), aliases);
+      });
+    });
+  });
+
+  describe("replaceAlias()", () => {
+    const cwd = "/proj";
+    const fileDir = "/proj/src/layerA";
+    const aliases = [{ alias: "@/", path: "./src/" }];
+    const testCases = [
+      { moduleSource: "@/layerA/layerAA", relPath: "./layerAA" },
+      { moduleSource: "nodeModule", relPath: "nodeModule" },
+    ];
+    testCases.forEach(({ moduleSource, relPath }) => {
+      it(`${moduleSource} -> ${relPath}`, () => {
+        assert.equal(
+          replaceAlias(cwd, fileDir, aliases)(moduleSource),
+          relPath
+        );
+      });
+    });
+  });
+
   describe("findLevel()", () => {
     const structure = [[{ name: "/src/layerA" }], [{ name: "/src/layerB" }]];
     const testCases = [
@@ -30,6 +95,30 @@ describe("helpers/lower-level-layer-imports", () => {
     testCases.forEach(({ path, level }) => {
       it(`The level of ${path} is ${level}`, () => {
         assert.equal(findLevel(structure)(path), level);
+      });
+    });
+  });
+
+  describe("createModulePath()", () => {
+    const cwd = "/proj";
+    const fileDir = "/proj/src/layerA";
+    const aliases = [{ alias: "@/", path: "./src/" }];
+    const testCases = [
+      {
+        moduleSource: "@/layerA/layerAA",
+        modulePath: "/proj/src/layerA/layerAA",
+      },
+      {
+        moduleSource: "nodeModule",
+        modulePath: "nodeModule",
+      },
+    ];
+    testCases.forEach(({ moduleSource, modulePath }) => {
+      it(`${moduleSource} -> ${modulePath}`, () => {
+        assert.equal(
+          createModulePath(cwd, fileDir, aliases)(moduleSource),
+          modulePath
+        );
       });
     });
   });
