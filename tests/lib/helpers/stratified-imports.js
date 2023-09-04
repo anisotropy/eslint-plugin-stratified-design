20b336ef5d31200639e7ac73c4ff2ec20ffc0115
Hodoug Joung <j.hodoug@example.com>
2023-09-04 22:42:17 +0900

    30903 no-disallowed-imports

--- tests/lib/helpers/stratified-imports.js
+++ tests/lib/helpers/stratified-imports.js
@@ -13,8 +13,6 @@ const {
   findLevel,
   findLayerWithReducedPath,
   toStructure,
-  createAliases,
-  replaceAlias,
   validateRawStructure,
   isInParent,
   isInChildren,
@@ -48,45 +46,6 @@ describe("helpers/stratified-imports", () => {
         structure
       )}`, () => {
         assert.deepEqual(toStructure(rawStructure, fileDir), structure);
-      });
-    });
-  });
-
-  describe("createAliases()", () => {
-    const testCases = [
-      {
-        rawAliases: { "@/": "./src/" },
-        aliases: [{ alias: "@/", path: "./src/" }],
-      },
-      {
-        rawAliases: { "@/": "./src/", "@layer/": "./src/layer/" },
-        aliases: [
-          { alias: "@layer/", path: "./src/layer/" },
-          { alias: "@/", path: "./src/" },
-        ],
-      },
-    ];
-    testCases.forEach(({ rawAliases, aliases }) => {
-      it(`${JSON.stringify(rawAliases)} -> ${JSON.stringify(aliases)}`, () => {
-        assert.deepEqual(createAliases(rawAliases), aliases);
-      });
-    });
-  });
-
-  describe("replaceAlias()", () => {
-    const cwd = "/proj";
-    const fileDir = "/proj/src/layerA";
-    const aliases = [{ alias: "@/", path: "./src/" }];
-    const testCases = [
-      { moduleSource: "@/layerA/layerAA", relPath: "./layerAA" },
-      { moduleSource: "nodeModule", relPath: "nodeModule" },
-    ];
-    testCases.forEach(({ moduleSource, relPath }) => {
-      it(`${moduleSource} -> ${relPath}`, () => {
-        assert.equal(
-          replaceAlias(cwd, fileDir, aliases)(moduleSource),
-          relPath
-        );
       });
     });
   });
