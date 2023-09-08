af6fec2e738b1f072cbd277934e82abc861671b5
Hodoug Joung <j.hodoug@example.com>
2023-09-09 00:35:43 +0900

    30903 finish initial version

---
+++ tests/lib/rules/no-diallowed-imports.js
@@ -0,0 +1,192 @@
+/**
+ * @fileoverview ...
+ * @author Hodoug Joung
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-disallowed-imports");
+const RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+});
+
+ruleTester.run("no-disallowed-imports", rule, {
+  valid: [
+    {
+      code: "import { foo } from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.js"],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "import { foo } from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["baz"], path: "**/src/fileC" },
+              allow: ["**/src/**/*.js"],
+            },
+            {
+              import: { specifiers: ["foo", "bar"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.js"],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "import { foo } from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo", "bar"], path: "**/src/fileA" },
+              disallow: ["**/src/**/*.test.js"],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "import { foo } from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo", "bar"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.js"],
+              disallow: ["**/src/**/*.test.js"],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "import { foo } from '@/fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.js"],
+            },
+          ],
+          aliases: { "@/": "./src/" },
+        },
+      ],
+    },
+    {
+      code: "import foo from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["default"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.js"],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  invalid: [
+    {
+      code: "import { foo } from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.test.js"],
+            },
+          ],
+        },
+      ],
+      errors: [
+        { messageId: "no-disallowed-imports", data: { specifier: "foo" } },
+      ],
+    },
+    {
+      code: "import { foo } from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo"], path: "**/src/fileA" },
+              disallow: ["**/src/**/*.js"],
+            },
+          ],
+        },
+      ],
+      errors: [
+        { messageId: "no-disallowed-imports", data: { specifier: "foo" } },
+      ],
+    },
+    {
+      code: "import { foo } from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.js"],
+              disallow: ["**/src/**/fileB.*"],
+            },
+          ],
+        },
+      ],
+      errors: [
+        { messageId: "no-disallowed-imports", data: { specifier: "foo" } },
+      ],
+    },
+    {
+      code: "import foo from './fileA'",
+      filename: "./src/fileB.js",
+      options: [
+        {
+          importPaths: [
+            {
+              import: { specifiers: ["foo"], path: "**/src/fileA" },
+              allow: ["**/src/**/*.js"],
+            },
+          ],
+        },
+      ],
+      errors: [
+        { messageId: "no-disallowed-imports", data: { specifier: "default" } },
+      ],
+    },
+  ],
+});
